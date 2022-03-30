6100f37ac8023371e4ce724dddaa3b710b634abe
Shun Yamazaki <syunn820@gmail.com>
2022-03-30 18:09:01 +0900

    refactor: カード部分を分離

--- pages/works.vue
+++ pages/works.vue
@@ -3,21 +3,7 @@
       <v-col cols="12" sm="8" md="6">
           <h1>Works</h1>
           <v-container v-for="app in items" :key="app">
-              <v-card>
-                  <v-card-title>{{app.name}}</v-card-title>
-                  <v-card-media>
-                      <v-img v-bind:src="app.img"></v-img>
-                      
-                  </v-card-media>
-                  <v-card-text>{{app.text}}</v-card-text>
-                  <v-card-subtitle>Platform: {{app.platform.join(", ")}}</v-card-subtitle>
-                  <v-card-subtitle>Status: {{app.status}}</v-card-subtitle>
-                  <v-divider></v-divider>
-                  <v-card-actions>
-                      <v-btn elevation="0" v-if="app.url != ''" :to="app.url"><span class="info--text">{{app.buttonName}}</span></v-btn>
-                      <v-btn elevation="0" v-if="app.outerurl != ''" :href="app.outerurl"><span class="info--text">{{app.buttonName}}</span></v-btn>
-                  </v-card-actions>
-              </v-card>
+              <WorkCard :work="app"></WorkCard>
           </v-container>
       </v-col>
   </v-row>
@@ -25,6 +11,8 @@
 
 <script>
 import RoutineTreeImage from "~/components/RoutineTreeImage.vue"
+import WorkCard from "../components/WorkCard.vue"
+
 export default {
     name: "WorksPage",
     data() {
@@ -53,6 +41,6 @@ export default {
             ]
         };
     },
-    components: { RoutineTreeImage }
+    components: { RoutineTreeImage, WorkCard }
 }
 </script>
