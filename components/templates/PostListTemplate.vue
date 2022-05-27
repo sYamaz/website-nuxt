276a7d6a21e4c6235de420d05d1e2f8a4b5c8fc5
Shun Yamazaki <syunn820@gmail.com>
2022-05-28 08:32:09 +0900

    refactor

---
+++ components/templates/PostListTemplate.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card elevation="1">
+    <ContentSection title="Posts">
+      <OGPCard
+        v-for="post in data.posts"
+        :key="post.id"
+        class="ma-4"
+        :url="post.url"
+        :title="post.title"
+        :description="post.body.slice(0, 140) + '...'"
+        site_name="qiita.com"
+        :image="require('~/static/qiita_logo-background-color.png')"
+      />
+    </ContentSection>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import { PostData } from '~/types/Qiita/Api/v2/datas'
+import OGPCard from '~/components/molecules/OGPCard.vue'
+import ContentSection from '~/components/organisms/ContentSection.vue'
+
+export interface PostListTemplateData {
+  posts: PostData[];
+}
+
+@Component({
+  components: {
+    OGPCard,
+    ContentSection
+  }
+})
+export default class PostListTemplate extends Vue {
+  @Prop({ default: undefined })
+  private data!: PostListTemplateData
+}
+</script>
+
+<style>
+</style>
