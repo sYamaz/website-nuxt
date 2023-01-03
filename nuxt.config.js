33319d6cab6e9dcb440ebc1bb3be851ee2072305
Shun Yamazaki <syunn820@gmail.com>
2023-01-03 14:10:21 +0900

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/apiPlugin.ts' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
