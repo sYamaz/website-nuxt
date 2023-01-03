31216f8a014951f6f3ccf44b1f62afd3b3c1489a
Shun Yamazaki <syunn820@gmail.com>
2023-01-03 15:25:33 +0900

    migrate: componentAPI => compositionAPI

--- pages/works.vue
+++ pages/works.vue
@@ -2,43 +2,35 @@
   <CardListViewTemplate :data="data" />
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
+<script lang="ts" setup>
 import CardListViewTemplate, {
   CardListViewTemplateData
 } from '../components/templates/CardListViewTemplate.vue'
 
-@Component({
-  components: {
-    CardListViewTemplate
-  }
-})
-export default class WorksPage extends Vue {
-  private data: CardListViewTemplateData = {
-    pageTitle: 'Works',
-    works: [
-      {
-        name: 'RoutineTree',
-        img: require('../static/RoutineTree.png'),
-        text: 'Task management application that helps you accomplish your daily routine tasks.',
-        platform: ['iOS'],
-        status: 'Released',
-        url: '',
-        outerurl: 'https://syamaz.github.io/RoutineTree/',
-        buttonName: 'Read more'
-      },
-      {
-        name: 'My homepage',
-        img: '',
-        text: 'This website.',
-        platform: ['Web'],
-        status: 'Released',
-        url: '/',
-        outerurl: '',
-        buttonName: 'Top page'
-      }
-    ]
-  }
+const data: CardListViewTemplateData = {
+  pageTitle: 'Works',
+  works: [
+    {
+      name: 'RoutineTree',
+      img: require('../static/RoutineTree.png'),
+      text: 'Task management application that helps you accomplish your daily routine tasks.',
+      platform: ['iOS'],
+      status: 'Released',
+      url: '',
+      outerurl: 'https://syamaz.github.io/RoutineTree/',
+      buttonName: 'Read more'
+    },
+    {
+      name: 'My homepage',
+      img: '',
+      text: 'This website.',
+      platform: ['Web'],
+      status: 'Released',
+      url: '/',
+      outerurl: '',
+      buttonName: 'Top page'
+    }
+  ]
 }
+
 </script>
